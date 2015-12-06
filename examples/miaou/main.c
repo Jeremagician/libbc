1c5c07ac1b9af4ba966a4bf9465a8470db867207
Jérémy Derdaele <jeremagician@gmail.com>
2015-12-06 17:54:55 +0100

    add miaou command line args
    miaou can now init an empty group and join an existing one.

--- examples/miaou/main.c
+++ examples/miaou/main.c
@@ -2,17 +2,57 @@
 #include <stdlib.h>
 #include <alloca.h>
 #include <bc.h>
-
 #include <string.h>
 #include <errno.h>
+#include <getopt.h>
 
-int main(void)
+void usage(char *prog)
+{
+	fprintf(stderr, "Usage: %s self-host [group-host]\n", prog);
+}
+
+char *split(char *str, char delim)
+{
+	for (; *str && *str != delim; str++)
+		;
+	return *str == delim ? (*str = '\0', ++str) : NULL;
+}
+
+int main(int argc, char *argv[])
 {
 	int ret;
-	struct bc_group *grp = alloca(bc_group_struct_size());
+	char *target_host = NULL, *target_port = NULL;
+	char *self_host = NULL, *self_port = NULL;
 	int chan[2];
+	struct bc_group *grp = alloca(bc_group_struct_size());
 
-	ret = bc_init(NULL, NULL, grp, chan);
+	if (argc < 2)
+	{
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* TODO(Jeremy): even if this format is handy, it does not support ipv6
+	 * rewrite something clever to handle it */
+	if (argc == 3)
+	{
+		target_host = argv[2];
+		target_port = split(argv[2], ':');
+	}
+
+	self_host = argv[1];
+	self_port = split(argv[1], ':');
+
+	if (argc == 3)
+	{
+		ret = bc_join(target_host, target_port,
+					  self_host, self_port, grp, chan);
+	}
+	else
+	{
+		ret = bc_init(self_host, self_port, grp, chan);
+	}
+
 	if (ret != 0)
 	{
 		fprintf(stderr, "Can't init %s\n", strerror(errno));
